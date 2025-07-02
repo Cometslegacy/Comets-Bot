0fe2d81b4be428a2bf28b44922b19cd523906a93
Cometslegacy <blackcomet9@gmail.com>
2025-07-02 10:00:00 -0400

    !horse

--- cogs/horse.py
+++ cogs/horse.py
@@ -5,10 +5,10 @@ class Horse(commands.Cog):
         def __init__(self, bot):
                 self.bot = bot
 
-                @commands.command()
-                async def horse(self, ctx):
-
-
+        @commands.command(name = "horse")
+        async def horse(self, ctx):
+                image = discord.File("resources\horse.png", filename="horse.png")
+                await ctx.send(file=image)
         
 async def setup(bot):
     await bot.add_cog(Horse(bot))
